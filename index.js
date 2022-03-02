b3d673b6f1f424c9b54c23413346d3b211b5e7e6
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-03-01 17:28:25 -0700

    added emplyee class

---
+++ index.js
@@ -0,0 +1,5 @@
+const Manager = require("./util/Manager")
+const Intern = require("./util/Intern")
+const Engineer = require("./util/Engineer")
+const fs = require('fs')
+const inquirer = require("inquirer")
