2c1c859dc2e067609aebe5c61bdae7c8bc5e59dc
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-03-01 17:42:06 -0700

    began inital function to determine type of worker

--- index.js
+++ index.js
@@ -3,3 +3,30 @@ const Intern = require("./util/Intern")
 const Engineer = require("./util/Engineer")
 const fs = require('fs')
 const inquirer = require("inquirer")
+const { existentialTypeParam } = require("babel-types")
+
+function init() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'type',
+            message: 'Are you a Manager, Intern, or Engineer?',
+            choices: ['Manager', 'Intern', 'Engineer', 'None']
+        }
+    ]).then(function(response) {
+        switch(response.type) {
+            case "Manager":
+                addManager()
+                break;
+            case "Intern":
+                addIntern()
+                break;
+            case "Engineer":
+                addEngineer()
+                break;
+            case "None":
+                exitApp()
+                break;
+        }
+    })
+}
